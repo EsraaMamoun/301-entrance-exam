a1b70f43463de1102c9aa81db15760bdaaf4c42b
EsraaMamoun <esraamamoun5@gmail.com>
2020-03-16 13:12:52 +0200

    Q1

---
+++ js/q1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var count = 0;
+function greaterThan(array, num) {
+    var array = [2, 7, 5, 9, 3];
+    var num = 5;
+    if (array[0] > num) {
+        count= count+1;
+    } else if (array[1] > num) {
+        count= count+1;
+    } else if (array[2] > num) {
+        count= count+1;
+    } else if (array[3] > num) {
+        count= count+1;
+    } else if (array[4] > num) {
+        count= count+1;
+    } else {
+        count = 0;
+    }
+    console.log(count);
+    return count;
+}
+
+greaterThan();
+
