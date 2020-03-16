e965716772bb10cde39771af7f83aa2eadcb34ee
EsraaMamoun <esraamamoun5@gmail.com>
2020-03-16 13:32:29 +0200

    Q2

---
+++ js/q2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+function stars() {
+   
+    var star = ['*','**','***','****','*****'];
+    for (let i = 0; i < star.length; i++) {
+     
+        console.log(star[i]);
+    }
+}
+stars();
